bdb93b1e6e30218bf56673429e99cdca3b66b325
Vitorino <deivisonjrvitorino@gmail.com>
2022-08-07 18:45:22 -0300

    Add files via upload

---
+++ exercicio_8_Transformar_o_estado_em_array_de_objeto/src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import CardPost from "./components/CardPost";
+
+
+function App() {
+  return (
+    <div>
+      <CardPost />
+    </div>
+  );
+}
+
+export default App;
---
+++ exercicio_8_Transformar_o_estado_em_array_de_objeto/src/components/CardPost/style.js
@@ -0,0 +1,48 @@
+import styled from "styled-components";
+
+export const WrapperCard = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  width: 25vw;
+  min-width:300px ;
+  height: 30vh;
+  border: 1px solid black;
+  border-radius:10px ;
+  line-height: 0;
+`;
+export const CardMain = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  max-height: 50%;
+
+  > img {
+    max-height:10vh ;
+  }
+`;
+
+export const CardComentarios = styled.div`
+  display: flex;
+  width: 100%;
+  justify-content: space-around;
+  align-items: center;
+`;
+export const CardPostContain = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: center;
+  max-width: 100vw;
+  height: auto;
+  gap: 20px;
+  input {
+    width: 80vw;
+    border-radius: 20px;
+    height: 40px;
+    padding-left: 20px;
+    border:none ;
+    box-shadow:1px 2px 5px 1px  ;
+  }
+`;
